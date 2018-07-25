17fcce3340e4f21385178e096c9184534c134cab
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2018-07-25 15:41:27 +0900

    Added all the files

--- VARS.sh
+++ VARS.sh
@@ -1,4 +1,4 @@
 NODES_ARGS="raven[01-39] -x raven20"
 LOGS_DIR="/scratch/vicente/yarn"
 MASTER="raven"
-OUTPUTDIR="/home/vicente/Dropbox/runs"
+OUTPUTDIR="/home/vicente/Dropbox/runs/input"
--- velox-stats-lib.R
+++ velox-stats-lib.R
@@ -1,6 +1,20 @@
 require(ggplot2)
 require(scales)
 
+normalize_time_simple <- function(df) {
+  df$start_time <- as.POSIXlt(df$start_time, format="%H:%M:%S")
+  df$end_time   <- as.POSIXlt(df$end_time, format="%H:%M:%S")
+
+  first_time <- min(df$start_time)
+
+  df$start_time <- difftime(df$start_time, first_time, units='sec')
+  df$end_time <- difftime(df$end_time, first_time, units='sec')
+
+  df$diff <- df$end_time - df$start_time
+
+  df <<- df
+}
+
 normalize_time <- function(df, exps=unique(df$experiment)) {
   df$start_time <- as.POSIXlt(df$start_time, format="%H:%M:%S")
   df$end_time   <- as.POSIXlt(df$end_time, format="%H:%M:%S")
@@ -24,57 +38,58 @@ normalize_time <- function(df, exps=unique(df$experiment)) {
   df <<- df
 }
 
-cluster_waves <- function(df, waves=3, iterations=10000) {
-  exps=unique(df$experiment)
-
-  df$mean_time <- (df$start_time + df$end_time)/2
-  df$wave <- 0
-  lapply(exps, function(exp) {
-    df[which(df$experiment == exp),]$wave = kmeans(df[which(df$experiment == exp),]$start_time, centers=waves, iter=iterations)$cluster
-    df <<- df
-  })
-
-
-  df[which(df$dfs == 'velox'),]$wave = 1
-  df <<- df
-}
-
 remove_reduces <- function(df) {
-  df <- df[which(df$type == 'MAP'),]
+  df <- df[which(df$type == 'm'),]
   df <<- df
 }
 
-cluster_set <- function(df) {
-  exps=unique(df$experiment)
+compute_waves <- function(df, waves=3, iterations=10000) {
+  df <- df[which(df$type == 'm'),]
+  print(head(df))
+  attach(df)
+  df$wave <- 1
 
-   waves_df <- data.frame()
+  df[which(df$dfs == 'hdfs'),]$wave <- kmeans(df[which(df$dfs == 'hdfs'),]$start_time, centers=3, iter=1000000)$cluster
 
-  lapply(exps, function(exp) {
-    waves=unique(df[which(df$experiment == exp),]$wave)
-    print(waves)
 
-    lapply(waves, function(wave) {
-      mi <- min(df[which(df$experiment == exp & df$wave == wave),]$start_time)
-      ma <- max(df[which(df$experiment == exp & df$wave == wave),]$end_time)
+  print(df[which(df$dfs == 'hdfs'),]$wave)
 
-      waves_df <- rbind(waves_df, data.frame(wave=wave, job=exp, min_time=mi, max_time=ma))
-      waves_df <<- waves_df
-    })
-    waves_df <<- waves_df
-  })
+  #print(aggregate(start_time ~ experiment, data=df[which(df$dfs == 'hdfs'),], FUN=function(start_time) {
+  #              return(kmeans(start_time, centers=3, iter=100000))
+  #}))
 
+  col_min <- aggregate(start_time, by=list(experiment,df$wave), min)
+  col_max <- aggregate(end_time, by=list(experiment,df$wave), max)
+  col_waves <- aggregate(df$wave, by=list(experiment,df$wave), mean)
+
+  waves_df <- data.frame(jobs=col_min$Group.1, waves=col_waves$x, min_time=col_min$x, max_time=col_max$x)
+  waves_df <- waves_df[order(waves_df$min_time),]
+
+  detach(df)
   waves_df <<- waves_df
 }
 
-waves_plot <- function(df) {
+waves_plot <- function(waves) {
   FONT_SIZE_X=12
   FONT_SIZE_Y=4
 
-  p <- ggplot() + 
-    geom_rect(data=df, aes(ymin=as.numeric(row.names(df)) - 0.5, ymax=as.numeric(row.names(df)), xmin=min_time, xmax=max_time, fill=job),color = "red",size = 0) +
-    scale_x_discrete(name="seconds", limits=seq(0,max(df$max_time),10)) +
-    theme(axis.text.x = element_text(size=8)) +
-		theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) 
+  l=c('4mb', '8mb','16mb', '32mb', '64mb', '128mb', '128mb', '128mb', '128mb')
+
+  idx=sort(as.numeric(row.names(waves)))
+
+  earliest=min(waves$max_time)
+
+  p <- ggplot() +
+    geom_rect(data=waves, aes(ymin=idx - 0.2, ymax=idx+ 0.2, xmin=min_time, xmax=max_time, fill=dfs),color = "black") +
+    geom_label(data=waves, aes(x = (max_time +min_time)/2, y = idx, label=l), fill="transparent", color="white") +
+    #geom_vline(xintercept=max(earliest), size=1) +
+    scale_x_discrete(name="Seconds", limits=seq(0,max(waves$max_time),10)) +
+    ggtitle("Waves execution time") +
+    guides(fill=guide_legend(title="FileSystem")) +                                                                          # Leyend title
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +                                          # Remove Grid
+    theme(axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) +                         # Remove y axis
+    theme(plot.title = element_text(hjust = 0.5)) +
+    scale_fill_grey()                                                                                                        # Grey bins
 
   return(p)
 }
@@ -84,18 +99,16 @@ tasks_plot <- function(df, exp) {
   print(summary(df$diff))
   FONT_SIZE_X=12
   FONT_SIZE_Y=4
-  p <- ggplot() + 
+  p <- ggplot() +
     geom_rect(data=df, aes(ymin=task_name, ymax=task_name, xmin=start_time, xmax=end_time),color = "red",size = 1) +
     geom_vline(xintercept=max(df$end_time), size=2, color=4) +
     scale_x_discrete(name="seconds", limits=seq(1,max(df$end_time),5)) +
 
     theme_gray() +
-    theme(text=  element_text(size=FONT_SIZE_Y)) + 
+    theme(text=  element_text(size=FONT_SIZE_Y)) +
     theme(axis.text.x = element_text(size=FONT_SIZE_X)) +
     theme(plot.title = element_text(size=28)) +
     ggtitle(exp)
 
   return(p)
 }
-
-
